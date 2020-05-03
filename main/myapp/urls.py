6417968d62221bc1ff403437d996fb1334624952
Yuvraj <jubrajmohanty@gmail.com>
2020-05-03 14:44:48 +0530

    first commit

---
+++ main/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('',views.index,name='home'),
+    path('add',views.add,name='add')
+
+]
